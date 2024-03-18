12696f417c605655ff5d614f70d5161c3a0b1382
wirabudhi <bpantaiselatan@gmail.com>
2024-03-19 01:37:53 +0800

    Add PelangganForm component and view

--- app/Livewire/PelangganForm.php
+++ app/Livewire/PelangganForm.php
@@ -2,12 +2,62 @@
 
 namespace App\Livewire;
 
+use App\Models\Pelanggan;
+use App\Models\User;
 use Livewire\Component;
+use LivewireUI\Modal\ModalComponent;
+use Masmerise\Toaster\Toastable;
 
-class PelangganForm extends Component
+class PelangganForm extends ModalComponent
 {
+    use Toastable;
+
+    public Pelanggan $pelanggan;
+    public $id, $user_id, $alamat, $no_hp, $users;
+
+    public function mount($rowId = null)
+    {
+        $this->pelanggan = Pelanggan::findOrNew($rowId);
+        $this->users = User::query()->whereHas('roles', function ($query) {
+            $query->where('name', 'Pelanggan');
+        })->get();
+        $this->id = $this->pelanggan->id;
+        $this->user_id = $this->pelanggan->user_id;
+        $this->alamat = $this->pelanggan->alamat;
+        $this->no_hp = $this->pelanggan->no_hp;
+    }
+
     public function render()
     {
         return view('livewire.pelanggan-form');
+    }
+
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'alamat' => 'required',
+            'no_hp' => 'required',
+        ];
+    }
+
+    public function resetForm()
+    {
+        $this->reset(['id', 'user_id', 'alamat', 'no_hp']);
+    }
+
+    public function store()
+    {
+        $validatedData = $this->validate();
+
+        $this->pelanggan = Pelanggan::updateOrCreate(['id' => $this->id], $validatedData);
+
+        $this->closeModalWithEvents([
+            PelangganTable::class => 'pelangganUpdated',
+        ]);
+
+        $this->success($this->pelanggan->wasRecentlyCreated ? 'Pelanggan berhasil ditambahkan' : 'Pelanggan berhasil diubah');
+
+        $this->resetForm();
     }
 }
--- resources/views/livewire/pelanggan-form.blade.php
+++ resources/views/livewire/pelanggan-form.blade.php
@@ -1,3 +1,52 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <form>
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div class="">
+                <div class="mb-4">
+                    <label for="exampleFormControlInput4" class="block text-gray-700 text-sm font-bold mb-2">Nama
+                        User</label>
+                    <select wire:model="user_id" class="select select-bordered w-full" id="exampleFormControlInput4">
+                        <option value="">Pilih User</option>
+                        @foreach ($users as $user)
+                            <option value="{{ $user->id }}">
+                                {{ $user->name }}</option>
+                        @endforeach
+                    </select>
+                    @error('user_id')
+                        <span class="text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="exampleFormControlInput1"
+                        class="block text-gray-700 text-sm font-bold mb-2">Alamat</label>
+                    <input type="text" class="input input-bordered w-full" id="exampleFormControlInput1"
+                        placeholder="Enter Alamat" wire:model="alamat">
+                    @error('alamat')
+                        <span class="text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="exampleFormControlInput2" class="block text-gray-700 text-sm font-bold mb-2">No
+                        Hp</label>
+                    <input type="text" class="input input-bordered w-full" id="exampleFormControlInput2"
+                        placeholder="Enter No Hp" wire:model="no_hp">
+                    @error('no_hp')
+                        <span class="text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+        </div>
+        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+                <button wire:click.prevent="store()" type="button" class="btn btn-success text-white">
+                    Submit
+                </button>
+            </span>
+            <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+                <button wire:click="closeModal()" type="button" class="btn btn-neutral">
+                    Close
+                </button>
+            </span>
+        </div>
+    </form>
 </div>
