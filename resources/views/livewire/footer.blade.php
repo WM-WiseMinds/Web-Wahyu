8b3143437d8990ab58f10367703d1f88bd1c0160
wirabudhi <bpantaiselatan@gmail.com>
2024-03-27 02:45:08 +0800

    Update guest layout with additional elements

---
+++ resources/views/livewire/footer.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <footer class="footer footer-center p-10 bg-gradient-to-t from-orange-700 to-orange-600 text-base-content rounded">
+        <nav class="grid grid-flow-col gap-4 text-white">
+            <a class="link link-hover" href="{{ url('/') }}">Beranda</a>
+            <a class="link link-hover" href="{{ url('/mobils') }}">Mobil</a>
+        </nav>
+        <aside class="text-white">
+            <p>Copyright © 2024 - All right reserved by PT Karya Marga Jaya</p>
+        </aside>
+    </footer>
+</div>
---
+++ resources/views/livewire/navbar.blade.php
@@ -0,0 +1,95 @@
+<div class="navbar bg-gradient-to-t from-orange-700 to-orange-600">
+    <div class="navbar-start">
+        <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#FFFF">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
+            </div>
+            <ul tabindex="0"
+                class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-black">
+                <li><a href="{{ url('/') }}">Beranda</a></li>
+                <li><a href="{{ url('/mobils') }}">Mobil</a></li>
+            </ul>
+        </div>
+        <a class="btn btn-ghost text-xl text-white">PT Karya Marga Jaya</a>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal px-1 text-white">
+            <li><a href="{{ url('/') }}">Beranda</a></li>
+            <li><a href="{{ url('/mobils') }}">Mobil</a></li>
+        </ul>
+    </div>
+    <div class="navbar-end">
+        @if (Route::has('login'))
+            @auth
+                <a href="{{ route('keranjang') }}" class="mx-3"><svg xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24" fill="#FFFF" class="w-6 h-6">
+                        <path
+                            d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+                    </svg>
+                </a>
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                            <button
+                                class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
+                                    alt="{{ Auth::user()->name }}" />
+                            </button>
+                        @else
+                            <span class="inline-flex rounded-md">
+                                <button type="button"
+                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                    {{ Auth::user()->name }}
+
+                                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </button>
+                            </span>
+                        @endif
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <!-- Account Management -->
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            {{ __('Manage Account') }}
+                        </div>
+
+                        <x-dropdown-link href="{{ route('profile.show') }}">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                            <x-dropdown-link href="{{ route('api-tokens.index') }}">
+                                {{ __('API Tokens') }}
+                            </x-dropdown-link>
+                        @endif
+
+                        <div class="border-t border-gray-200"></div>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}" x-data>
+                            @csrf
+
+                            <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+            @else
+                <a href="{{ route('login') }}" class="btn btn-primary mx-1 text-white">Log
+                    in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="btn btn-ghost mx-1 text-white">Register</a>
+                @endif
+            @endauth
+        @endif
+    </div>
+
+</div>
